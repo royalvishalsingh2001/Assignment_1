e2df187a6937d50a7d7961dde403ce5b97106cc4
Vishal Singh <devvishalsingh2001@gmail.com>
2025-08-13 02:15:56 +0530

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,5 @@
+first=input("Enter your first name :")
+last=input("Enter your last name :")
+sum=first+last
+greet=(f"Hello, {sum} Welcome to the python program.")
+print(greet)
---
+++ mathematic.py
@@ -0,0 +1,12 @@
+operator_1=int(input("enter your number 1 :"))
+operator_2=int(input("enter your number 2 :"))
+print("+,-,*,/")
+add=operator_1+operator_2
+sub=operator_1-operator_2
+mul=operator_1*operator_2
+div=operator_1/operator_2
+print(f"addition :{add}")
+print(f"subtraction :{sub}")
+print(f"multiplication :{mul}")
+print(f"division :{div}")
+print("Thank U")
